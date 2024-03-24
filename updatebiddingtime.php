15a526adff9671da4a3719dde681ce59d6b1629e
u1804025 <111126924+u1804025@users.noreply.github.com>
2024-03-25 01:34:23 +0600

    bidding time update

---
+++ updatebiddingtime.php
@@ -0,0 +1,73 @@
+<?php
+session_start();
+if(!isset($_SESSION['email'])){
+    header('location:signinedit.php');
+}
+else{
+$email=$_SESSION['email'];
+}
+$link=mysqli_connect("localhost","root","");
+mysqli_select_db($link,"salescom");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="uploadnew.css">
+    <title>signup</title>
+</head>
+<body>
+    <div class="signupbox">
+        <h1>Update Your Products' Bidding time</h1>
+        
+        <form method="post" action="" enctype="multipart/form-data">
+            <table>
+               
+                
+                <tr>
+                    <td>Bid Start Time</td>
+                    <td><input type="datetime-local" name="bst" required></td>
+                </tr>
+                <tr>
+                    <td>Bid End Time</td>
+                    <td><input type="datetime-local" name="bet" required></td>
+                </tr>
+                
+                <tr style="padding: 10px ;">
+                    
+                    <td colspan="2" ><input type="submit"name="submit1"value="Upload"></td>
+                </tr>
+                
+            </table>
+            
+        </form>
+        <?php
+        if(isset($_POST["submit1"])){
+            $v1=rand(1111,999);
+            $v2=rand(1111,999);
+            $v3=$v1.$v2;
+            $v3=md5($v3);
+            $fnm=$_FILES["pimage"]["name"];
+            $dst="./productimage/".$v3.$fnm;
+            $dst1="productimage/".$v3.$fnm;
+            move_uploaded_file($_FILES["pimage"]["tmp_name"],$dst);
+            $pnm = filter_input(INPUT_POST, 'pnm'); 
+            $pprice = filter_input(INPUT_POST, 'pprice');
+            $pimage = filter_input(INPUT_POST, 'dst1');
+            $pcategory = filter_input(INPUT_POST, 'pcategory');
+            $pdesc = filter_input(INPUT_POST, 'pdesc');
+            $bst=filter_input(INPUT_POST, 'bst');
+            $bet=filter_input(INPUT_POST, 'bet');
+            date_default_timezone_set('Asia/Dhaka');
+            mysqli_query($link,"INSERT INTO productnew values('$email','','$pnm','$pprice','$dst1','$pcategory','$pdesc','$bst','$bet')");
+            header('location:product.php');
+            mysqli_query($link,"INSERT INTO bidding values('','','0','$pprice','$pprice','$bst','$bet')");
+            header('location:product.php');
+        }
+        ?>
+    </div>
+            
+</body>
+</html>
